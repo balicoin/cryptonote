33d0e28b3db922dace9ef875026e6179629eee1d
Bali Coin <widiastawan@icloud.com>
2017-06-27 11:56:17 +0800

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -17,8 +17,8 @@ const uint64_t DIFFICULTY_TARGET = 133;
 const int P2P_DEFAULT_PORT = 13333;
 const int RPC_DEFAULT_PORT = 12333;
 const std::initializer_list<const char*> SEED_NODES = {
-  "111.11.11.11:17236",
-  "222.22.22.22:17236",
+  "dns1.masterwebnet.com:13333",
+  "dns2.masterweb.net:13333",
 };
  
 const uint64_t MINIMUM_FEE = 100000;
